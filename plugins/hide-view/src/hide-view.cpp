4b7011163bf2b9a65c0b2489297c523e1b0fc5db
killown <systemofdown@gmail.com>
2024-08-16 02:25:58 -0300

    Update hide-view.cpp

--- plugins/hide-view/src/hide-view.cpp
+++ plugins/hide-view/src/hide-view.cpp
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2024 Thiago
+Copyright (c) 2024 Thiago <systemofdown@gmail.co>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
